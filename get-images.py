50fda96e82aac29c4f64a081d17dabbf336b4abc
Cooper Hammond <kepoorh@gmail.com>
2020-05-28 23:29:07 -0700

    get-images.py

--- get-images.py
+++ get-images.py
@@ -3,8 +3,10 @@ import os
 import subprocess
 from urllib.parse import urlencode
 
-from fastai.vision import *
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+from fastai.vision import *
 
 
 CHROME_DRIVER_LOC = r'C:\Users\C23Cooper.Hammond\Documents\dev\bin\chromedriver.exe'
@@ -12,10 +14,13 @@ CHROME_DRIVER_LOC = r'C:\Users\C23Cooper.Hammond\Documents\dev\bin\chromedriver.
 
 class ContentGrabber:
 
-    def __init__(self, classes, queries, destination):
+    def __init__(self, classes, queries, destination, blocks=4):
         self.classes = classes
         self.queries = queries
         self.destination = Path(destination)
+        self.blocks = blocks
+
+        self.wsl_folder = subprocess.check_output('wslpath "%s"' % self.destination, shell=True).decode('utf-8').strip("\n")
 
         root_url = 'https://www.google.com/search?tbm=isch&'
         self.image_url_queries = []
@@ -24,44 +29,50 @@ class ContentGrabber:
             self.image_url_queries.append(url)
 
     def grab_images(self):
-        get_image_urls_script = "urls=Array.from(document.querySelectorAll('.rg_i')).map(el=> el.hasAttribute('data-src')?el.getAttribute('data-src'):el.getAttribute('data-iurl'));" + \
-                                "\n" + "window.open('data:text/csv;charset=utf-8,' + escape(urls.join('\\n')));"
+        self.grab_urls()        
+        
+        ## DOWNLOAD IMAGES ##
+        for c in self.classes:
+            print(c)
+            class_data_dest = Path(self.wsl_folder + "/" + c)
+            class_data_dest.mkdir(parents=True, exist_ok=True)
+            download_images(self.wsl_folder + ('/%s.csv' % c), class_data_dest)
+
+        for c in self.classes:
+            print(c)
+            verify_images(Path(self.wsl_folder + "/" + c), delete=True, max_size=1000)
+
+    def grab_urls(self):
+        get_image_urls_script = \
+            "urls=Array.from(document.querySelectorAll('.rg_i')).map(el=> el.hasAttribute('data-src')?el.getAttribute('data-src'):el.getAttribute('data-iurl'));" + \
+            "\n" + "window.open('data:text/csv;charset=utf-8,' + escape(urls.join('\\n')));"
 
         options = webdriver.ChromeOptions()
         options.add_experimental_option('prefs', {'download.default_directory' : str(self.destination)})
 
-        ## GRAB URLS ##
         os.system('cmd.exe /c "%s" &' % CHROME_DRIVER_LOC) # spawns chromedrive instance
 
         driver = webdriver.Chrome(port=9515, options=options)
 
         for url in self.image_url_queries:
             driver.get(url)
+            for i in range(0, self.blocks):
+                html = driver.find_element_by_tag_name('html')
+                html.send_keys(Keys.END)
+                time.sleep(2)
             driver.execute_script(get_image_urls_script)
 
         time.sleep(2)
         driver.quit()
 
         ## RENAME URL FILES ##
-        wsl_downloads_folder = subprocess.check_output('wslpath "%s"' % self.destination, shell=True).decode('utf-8').strip("\n")
         for index, c in enumerate(self.classes):
             filename = 'download'
             if index > 0:
                 filename += ' (%s)' % index
-            os.rename(wsl_downloads_folder + '/%s' % filename, 
-                      wsl_downloads_folder + '/%s.csv' % c)
+            os.rename(self.wsl_folder + '/%s' % filename, 
+                      self.wsl_folder + '/%s.csv' % c)
             index += 1
-        
-        ## DOWNLOAD IMAGES ##
-        for c in self.classes:
-            print(c)
-            class_data_dest = Path(wsl_downloads_folder + "/" + c)
-            class_data_dest.mkdir(parents=True, exist_ok=True)
-            download_images(wsl_downloads_folder + ('/%s.csv' % c), class_data_dest)
-
-        for c in self.classes:
-            print(c)
-            verify_images(Path(wsl_downloads_folder + "/" + c), delete=True, max_size=1000)
 
 
 def main():
@@ -73,9 +84,9 @@ def main():
         'pizza'
     ]
     queries = [
-        'hamburger',
-        'sandwich',
-        'pizza'
+        '"hamburger" -sandwich',
+        '"sandwich" -hamburger',
+        '"pizza"'
     ]
 
     g = ContentGrabber(classes, queries, downloads_folder)
